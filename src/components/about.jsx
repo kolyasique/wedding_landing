864e5f754a88cf5284e9cd99eef108a5e07ae954
Nikolai Filippov <nikolayvadimovich@icloud.com>
2024-05-28 14:30:21 +0300

    test

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,43 +1,57 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 export const About = (props) => {
-  return (
-    <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.id = 'weatherwidget-io-js';
+		script.src = 'https://weatherwidget.io/js/widget.min.js';
+		script.async = true;
+
+		document.getElementsByTagName('head')[0].appendChild(script);
+
+		return () => {
+			if (script.parentNode) {
+				script.parentNode.removeChild(script);
+			}
+		};
+	}, []);
+	return (
+		<div id="about">
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-12 col-md-6">
+						{' '}
+						<img src="img/wedding/elegance2.jpeg" className="img-responsive" alt="" />{' '}
+					</div>
+					<div className="col-xs-12 col-md-6">
+						<div className="about-text">
+							<h2>Прогулка на яхте Elegance</h2>
+							<p>{props.data ? props.data.paragraph : 'Загрузка...'}</p>
+							<h3>Важные моменты</h3>
+							<div className="list-style">
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>{props.data ? props.data.Why.map((d, i) => <li key={`${d}-${i}`}>{d}</li>) : 'loading'}</ul>
+								</div>
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>{props.data ? props.data.Why2.map((d, i) => <li key={`${d}-${i}`}> {d}</li>) : 'loading'}</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="forecast_cover">
+					<a
+						className="weatherwidget-io"
+						href="https://forecast7.com/ru/60d0830d12/saint-petersburg/"
+						data-label_1="Санкт-Петербург"
+						data-label_2="Погода"
+						data-theme="pure"
+						disabled
+					>
+						Санкт-Петербург Погода
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,36 +1,30 @@
-import { Image } from "./image";
-import React from "react";
+import { Image } from './image';
+import React from 'react';
 
 export const Gallery = (props) => {
-  return (
-    <div id="portfolio" className="text-center">
-      <div className="container">
-        <div className="section-title">
-          <h2>Gallery</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
-        </div>
-        <div className="row">
-          <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
-              : "Loading..."}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="portfolio" className="text-center">
+			<div className="container">
+				<div className="section-title">
+					<h2>Меню</h2>
+					<p>
+						Выберите сет, который вам понравится, и мы закажем его под вас. Если есть аллергия на выбранные продукты, просьба
+						сообщить и мы согласуем блюда
+					</p>
+				</div>
+				<div className="row">
+					<div className="portfolio-items">
+						{props.data
+							? props.data.map((d, i) => (
+									// <div key={`${d.title}-${i}`} className="col-sm-2 col-md-4 col-lg-4">
+									<div key={`${d.title}-${i}`} className="portfolio-item">
+										<Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+									</div>
+							  ))
+							: 'Loading...'}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
